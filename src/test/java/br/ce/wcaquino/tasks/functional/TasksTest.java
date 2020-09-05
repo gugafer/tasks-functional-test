ea653f102fa666b68b0b5088409ef5cfc6c3f08b
Gustavo Ferreira <gu.u@hotmail.com>
2020-09-04 21:04:26 -0300

    paralelismo em docker

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -1,25 +1,50 @@
 package br.ce.wcaquino.tasks.functional;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest {
 
-	public WebDriver acessarAplicacao () {
-		WebDriver driver = new ChromeDriver();
-		driver.navigate().to("http://127.0.0.1:8001/tasks/");
+	public WebDriver acessarAplicacao () throws MalformedURLException {
+//		WebDriver driver = new ChromeDriver();
+//		DesiredCapabilities cap = DesiredCapabilities.chrome();
+//		WebDriver driver = RemoteWebDriver(new URL("http://192.168.100.4:4444/wd/hub"), cap);
+		//WebDriver driver = new RemoteWebDriver("http://192.168.100.4:4444/wd/hub", cap);
+//		WebDriver driver = new RemoteWebDriver(new URL("http://192.168.100.4:4444/wd/hub", cap));
+/*        WebDriver driver;
+    driver = RemoteWebDriver(new URL("http://192.168.100.4:4444/wd/hub"), cap);*/
+		ChromeOptions cap = new ChromeOptions(); 
+		cap.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR,
+		                  UnexpectedAlertBehaviour.IGNORE);
+
+		org.openqa.selenium.remote.RemoteWebDriver driver = new RemoteWebDriver(new URL("http://192.168.100.4:4444/wd/hub"),cap);
+		
+		driver.navigate().to("http://192.168.100.4:8001/tasks/");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
 	
+
+	public WebDriver RemoteWebDriver(URL url, DesiredCapabilities cap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 	@Test
-	public void deveSalvarTarefaComSucesso() {
+	public void deveSalvarTarefaComSucesso() throws MalformedURLException {
 
 		WebDriver driver = acessarAplicacao();
 		try {
@@ -43,7 +68,7 @@ public class TasksTest {
 	}
 
 	@Test
-	public void naoDeveSalvarTarefaSemDescricao() {
+	public void naoDeveSalvarTarefaSemDescricao() throws MalformedURLException {
 
 		WebDriver driver = acessarAplicacao();
 		try {
@@ -66,7 +91,7 @@ public class TasksTest {
 
 	
 	@Test
-	public void naoDeveSalvarTarefaSemData() {
+	public void naoDeveSalvarTarefaSemData() throws MalformedURLException {
 
 		WebDriver driver = acessarAplicacao();
 		try {
@@ -88,7 +113,7 @@ public class TasksTest {
 	}
 
 	@Test
-	public void naoDeveSalvarTarefaComDataPassada() {
+	public void naoDeveSalvarTarefaComDataPassada() throws MalformedURLException {
 
 		WebDriver driver = acessarAplicacao();
 		try {
